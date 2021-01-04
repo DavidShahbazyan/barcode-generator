606db862e13424fac97fe0d1fceec3f7448c099a
David Shahbazyan <DavidShahbazyan@users.noreply.github.com>
2021-01-04 17:22:25 +0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+/**
+ * Author: David Shahbazyan
+ * Company: DavSoft
+ * Date: Jan 03, 2021
+ * Time: 9:39 PM
+ */
+module barcodegenerator {
+    requires com.google.zxing;
+    requires org.jfree.jfreesvg;
+    requires java.desktop;
+    requires org.reflections.common;
+
+    exports am.davsoft.barcodegenerator;
+    exports am.davsoft.barcodegenerator.api;
+    exports am.davsoft.barcodegenerator.api.barcodedata;
+    exports am.davsoft.barcodegenerator.builder;
+    exports am.davsoft.barcodegenerator.builder.barcodedata;
+    exports am.davsoft.barcodegenerator.impl;
+    exports am.davsoft.barcodegenerator.impl.barcodedata;
+    exports am.davsoft.barcodegenerator.helper;
+}
